d8fb0d65970bf075a6f64b538e561a8af462f76a
Jan Cankusic <jancankusic26@gmail.com>
2020-05-17 18:56:35 -0700

    first way to filter monster search

--- src/App.js
+++ src/App.js
@@ -2,31 +2,42 @@ import React from 'react';
 import { Component } from 'react'; 
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import Search from './components/Search/search.components'
 
 export default class App extends Component {
   constructor() {
     super(); 
     this.state = {
-       monsters: []
+       monsters: [],
+       renderedMonsters: []
     }
   }
 
   componentDidMount() {
-    const loadData = json => this.setState({ monsters: json}) 
+    const loadData = json => this.setState({ monsters: json}); 
+    const loadRenderedMonsters = () => {this.setState({ renderedMonsters: this.state.monsters})};
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(loadData)
+    .then(loadData).then(loadRenderedMonsters);
   }
 
-  searchMonsters (searchInput) => {
+searchMonsters = e => {
+  this.setState({ renderedMonsters: this.state.monsters.filter(monster => monster.name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1)})
+    
+    
+    
+    // console.log(monster.name)
+    // console.log(e.target.value)
+    // console.log(monster.name.indexOf(e.target.value)) 
+    // return monster.name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1
 
-  }
+}
 
   render() {
     return(
       <div className='app'>
-        <Search serachMonsters={this.searchMonsters} />
-        <CardList monsters={this.state.monsters}></CardList>
+        <Search searchMonsters={this.searchMonsters} />
+        <CardList monsters={this.state.renderedMonsters}></CardList>
       </div>
     )
   }
---
+++ src/components/Search/search.components.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Search(props) {
+    return (
+        <input type='text' onChange={props.searchMonsters}/> 
+    )}
